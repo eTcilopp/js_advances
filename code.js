9afe924c64091687e13aa434f7f077d41e01cdc4
eTcilopp <eTcilopp@users.noreply.github.com>
2020-09-26 11:24:21 -0600

    Домашняя работа №4 - регулярные выражения.
    Домашняя работа №4 - регулярные выражения. Также доделано ДЗ №3.

--- code.js
+++ code.js
@@ -1,10 +1,6 @@
-const renderProduct = (title='Товар', price=5000) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p class='price'>${price}</p>
-                <button class="by-btn">Добавить в корзину</button>
-              </div>`;
-};
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+let catalogDict = {}; // формируется после получения данных о товарах с сервера. Используется для связи между кнопкой "добавить в корзину"
+                        // и объектом товара.
 
 class Basket {
     constructor (client){
@@ -12,25 +8,63 @@ class Basket {
         this.productList=[];
     }
 
+    renderBasket = function(){
+        let basketHTML=[`
+            <h2>Корзина (${this.client}).</h2>
+            <h3> Всего ${this.basketQuantity} товара(ов) на сумму ${this.basketPrice} &#8381</h3>
+            <a class="close" href="#">&times;</a>
+            <hr>
+            `
+        ];
+        this.productList.forEach(product => {
+            basketHTML += `
+            <div class="basket-item">
+                <h2>${product.title}</h2>
+                    <div class="basket-item-text">
+                        <p class='price'>Price: ${product.price} &#8381</p>
+                        <p> Quantity: ${product.quantity}</p>
+                    </div>
+                    <div class="basket-item-image">
+                        <img src=${product.image_url} alt="${product.title}"> 
+                    </div>
+                <hr>
+            </div>
+            `;
+        
+        });
+        return basketHTML;
+    }
+    
     addProduct = function(Product){
-        this.productList.push(Product)
-        // return this.productList
-        return
+        let productExist = false;
+        this.productList.forEach(product => {
+            if (product.id == Product.id){
+                product.quantity++;
+                productExist = true;
+                return;
+            }
+        })
+        if (!productExist){
+            this.productList.push(Object.assign({}, Product));
+        }
     }
 
     get basketQuantity() {
-        return this.productList.length;
+        let basketQuantity = 0;
+        this.productList.forEach(product => {
+            basketQuantity += product.quantity
+        })
+        return basketQuantity;
     }
 
     get basketPrice() {
         let total=0;
         this.productList.forEach(product => {
-            total += product.price
+            total += product.price * product.quantity
         });
         return total;
 
     }
-
 }
 
 
@@ -40,33 +74,95 @@ class Product {
         this.title=title;
         this.price=price;
         this.image_url=image_url;
+        this.quantity = 1;
     }
     renderProduct() {
-        return `<div class="product-item">
+        return `<div id="${this.id}" class="product-item" style="visibility:visible;">
         <img src=${this.image_url} alt=""> 
         <h3>${this.title}</h3>
         <p class='price'>${this.price} &#8381</p>
-        <button class="by-btn">Добавить в корзину</button>
+        <button class="by-btn button" id="${this.id}" >Добавить в корзину</button>
       </div>`;
     }
 
 }
 
-const product1 = new Product(1, 'Notebook', 20000)
-const product2 = new Product(2, 'Mouse', 1500)
-const product3 = new Product(3, 'Keyboard', 5000)
-const product4 = new Product(4, 'Printer', 4500)
 
-document.querySelector('.products').insertAdjacentHTML('beforeend', product1.renderProduct())
-document.querySelector('.products').insertAdjacentHTML('beforeend', product2.renderProduct())
-document.querySelector('.products').insertAdjacentHTML('beforeend', product3.renderProduct())
-document.querySelector('.products').insertAdjacentHTML('beforeend', product4.renderProduct())
+function makeGETRequest(url, callback) {
+    var xhr;
+  
+    if (window.XMLHttpRequest) {
+      xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { 
+      xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+  
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        callback(xhr.responseText);
+      }
+    }
+    xhr.open('GET', url, true);
+    xhr.send();
+  }
+  
+// Функция для получения списка товаров с сервера, формирования словаря-каталога с этим списком ({id товара: объект товара}),
+// отрисовывания товаров на странице
+function fetchGoods(){
+    makeGETRequest(`${API}/catalogData.json`, (items) => {
+        this.items = JSON.parse(items);
+        this.items.forEach(item => {
+            const product = new Product(item.id_product, item.product_name, item.price);
+            catalogDict[item.id_product] = product;
+            document.querySelector('.products').insertAdjacentHTML('beforeend', product.renderProduct());
+        }
+        )
+    }
+    )
+}
 
-const basket1 = new Basket('user1')
-basket1.addProduct(product1)
-basket1.addProduct(product2)
-basket1.addProduct(product4)
+// Навешиваем события клика на кнопку "Добавить в корзину"
+function events () {
+    document.querySelectorAll('.by-btn').forEach(button => {
+        button.addEventListener('click', event => {
+            basket.addProduct(catalogDict[event.target.id])
 
-console.log('Quantity total: ' + basket1.basketQuantity) //Quantity total: 3
-console.log('Price total: ' + basket1.basketPrice) //Price total: 26000
+        })
+    })
+}
 
+// я предполагаю, что пользователь должен появиться после авторизации, которой у нас (пока) нет. Поэтому делаю вручную.
+const basket = new Basket('Anonimus');
+
+// на клик по кнопке "корзина" навешено событие - отрисовать корзину.
+document.getElementById('basket_button').addEventListener('click', event => document.querySelector('.basket_content').innerHTML=basket.renderBasket());
+
+
+// Promise я не смог победить. Цепочка срабатывает, но без костыля в виде задержки (последняя строка) events() не находит ни одной кнопки.
+// Если у Вас будет минута свободная, напишите, пожалуйста, как нужно то было?
+const fetchPromise = new Promise((resolve)=>{fetchGoods()});
+fetchPromise.then(response => {
+    return response.json();
+  }).then(setTimeout(()=> {events()}, 800));
+
+// Событие клика на кнопку поиска
+
+document.getElementsByClassName('searchButton')[0].addEventListener('click', event =>searchField())
+let field = document.getElementsByClassName('searchTerm')[0]
+field.addEventListener("keyup", event =>{
+    if (event.keyCode === 13) {
+        searchField()
+    }
+})
+function searchField(){
+    let searchText = field.value;
+    let re = new RegExp(searchText, 'i');
+    for (let id in catalogDict){
+        if (catalogDict[id].title.match(re)){
+            document.getElementById(id).style.visibility='visible';
+        }else{
+            document.getElementById(id).style.visibility='hidden';
+        }
+    }
+
+}
